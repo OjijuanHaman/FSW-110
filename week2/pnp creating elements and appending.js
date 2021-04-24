e8dd972e69c30dbb57190e202f74113fd15a0bb8
OjijuanHaman <ojijuan.haman@bryanuniversity.edu>
2021-04-24 07:02:49 -0500

    completed pnp week2

---
+++ week2/pnp creating elements and appending.js
@@ -0,0 +1,36 @@
+
+var newHeading = document.createElement('h1')
+newHeading.textContent = 'Welcome to my JS site'
+
+document.body.appendChild(newHeading)
+
+var newP = document.createElement('p')
+newP.textContent = 'All of this was created with JavaScript'
+
+document.body.appendChild(newP)
+
+function orderedList(){
+
+var lists =[
+    'Greens',
+    'Beans',
+    'Potatoes',
+    'Tomatoes'
+],
+
+listContents = document.createElement('div'),
+
+list = document.createElement('ol'),
+itemsInList = lists.length,listItem,i;
+document.getElementsByTagName('body')[0].appendChild(listContents);
+listContents.appendChild(list);
+
+for (i = 0; i < itemsInList; ++i){
+    listItem = document.createElement('li');
+    listItem.innerHTML = lists[i];
+    list.appendChild(listItem)
+
+    }
+
+}
+orderedList();
