010d57cc3bc66cb8ffbc3c04625ceaf00cc63a09
OjijuanHaman <ojijuan.haman@bryanuniversity.edu>
2021-05-13 14:47:44 -0500

    deleted folder

--- week4/Project 2 Calculator.js
+++ week4/Project 2 Calculator.js
@@ -1,8 +1,9 @@
 
 
-//create 3 divs//
+
 var div1= document.createElement("div")
 document.body.appendChild(div1)
+div1.style.border="solid black 1px"
 var div2= document.createElement("div")
 document.body.appendChild(div2)
 var div3= document.createElement("div")
@@ -10,12 +11,23 @@ document.body.appendChild(div3)
 
 
 var input1 = document.createElement('input')
-document.body.appendChild(input1 )
+div1.appendChild(input1 )
 var input2 = document.createElement('input')
-document.body.appendChild(input2 )
+div1.appendChild(input2 )
 
 var addButton = document.createElement('button')
-document.body.appendChild(addButton)
+addButton.id = "add"
+addButton.addEventListener('click',function(){
+    
+    var adding = input1.value
+    var adding2 = input2.value
+    var sum = Number(adding2) + Number(adding)
+    
+    var addAnswer = document.createElement('p')
+    addAnswer.textContent = sum
+    div1.appendChild(addAnswer)
+})
+div1.appendChild(addButton)
 var subButton = document.createElement('button')
 document.body.appendChild(subButton)
 var mulButton = document.createElement('button')
