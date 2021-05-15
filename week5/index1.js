c645cfd62ca60796f10be38ea27f60dbffc5471e
OjijuanHaman <ojijuan.haman@bryanuniversity.edu>
2021-05-15 13:16:29 -0500

    added P&P 2 week5

---
+++ week5/index1.js
@@ -0,0 +1,24 @@
+function AlertForm(){
+  var a = document.stats.fname.value;  
+  var b = document.stats.lname.value;
+  var c = document.stats.age.value;  
+  var d = document.stats.answer.value;
+  var e = document.stats.color.value;
+  
+  var boxes = document.getElementsByName('h')
+  var checkedBoxes = ' ';
+  for (var i = 0; i < boxes.length; i ++){
+      if (boxes[i].type == 'checkbox' && boxes[i].checked == true){
+      checkedBoxes += boxes[i].value + ', ';
+      }
+    }
+        alert(a + checkedBoxes)
+}
+  
+
+
+
+
+    
+        
+    
